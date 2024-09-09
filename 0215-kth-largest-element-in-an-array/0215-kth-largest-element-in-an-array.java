f841740241289cc05b8ea4d4b3636645bcc552dc
Yingjie Yang <102939225+Tiffany678@users.noreply.github.com>
2024-09-09 15:13:41 -0400

    Time: 41 ms (62.65%), Space: 57.1 MB (84.15%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,19 +1,21 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
 
-        // create PriorityQueue to store elements from nums, so elements can be stored in ascending order in PriorityQueue
-        Queue<Integer> q = new PriorityQueue();
+        // create PriorityQueue, pg to store elements from nums array, so elements can be stored in ascending order.
+        Queue<Integer> pq = new PriorityQueue();
 
+        // Store K elements of the nums array
         for(int i=0;i<k;i++){
-            q.add(nums[i]);
+            pq.add(nums[i]);
         }
+
+        // iterate the remaind elements of array, and compair with the smallest element in pq. If the current element is larger than the pq's smallest element, remove it and add to the pq. so that, the pq can only contain size of k consistenly, and return the first element at the front with peek()
         for(int i = k;i<nums.length;i++){
-            if(nums[i]>q.peek()){
-                q.poll();
-                q.add(nums[i]);
+            if(nums[i]>pq.peek()){
+                pq.poll(); 
+                pq.add(nums[i]);
             }
         }
-        return q.peek();
-
+        return pq.peek();
     }
 }
